44ae050aab66644d4a542c2837c4623d9e3109f5
Pingplof <60694913+Pingplof@users.noreply.github.com>
2021-03-04 13:38:36 -0300

    trabalho atualizado

--- salvar-dados.php
+++ salvar-dados.php
@@ -1,5 +1,5 @@
 ﻿<?php
-
+	include "utils.php";
     $n = $_POST["n"];
     $data = $_POST["data"];
     $firma = $_POST["firma"];
@@ -28,79 +28,16 @@
         include "Formulario-cadastro-cliente.php";
         die;
     }
-
-
-	function validaCNPJ(cnpj == null){}
-	if(empty($cnpj)) {
-		echo "<b> CNPJ não valido </b>";
+	if(empty($cnpj)){
+		echo "<b> O campo CNPJ deve ser preenchido.<b>";
 		include "Formulario-cadastro-cliente.php";
 		die;
 	}
-
-	// Elimina possivel mascara
-	$cnpj = preg_replace("/[^0-9]/", "", $cnpj);
-	$cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
-	
-	// Verifica se o numero de digitos informados é igual a 11 
-	if (strlen($cnpj) != 14) {
-		echo "<b> CNPJ não valido </b>";
+	if(!validarCNPJ($cnpj)){
+		echo "<b>O cnpj invalido</b>";
 		include "Formulario-cadastro-cliente.php";
 		die;
 	}
-	
-	// Verifica se nenhuma das sequências invalidas abaixo 
-	// foi digitada. Caso afirmativo, retorna falso
-	else
-	if ($cnpj == '00000000000000' || 
-		$cnpj == '11111111111111' || 
-		$cnpj == '22222222222222' || 
-		$cnpj == '33333333333333' || 
-		$cnpj == '44444444444444' || 
-		$cnpj == '55555555555555' || 
-		$cnpj == '66666666666666' || 
-		$cnpj == '77777777777777' || 
-		$cnpj == '88888888888888' || 
-		$cnpj == '99999999999999') {
-		echo "<b> CNPJ não valido </b>";
-		include "Formulario-cadastro-cliente.php";
-		die;
-		
-	 // Calcula os digitos verificadores para verificar se o
-	 // CPF é válido
-	 } else {   
-	 
-		$j = 5;
-		$k = 6;
-		$soma1 = "";
-		$soma2 = "";
-
-		for ($i = 0; $i < 13; $i++) {
-
-			$j = $j == 1 ? 9 : $j;
-			$k = $k == 1 ? 9 : $k;
-
-			$soma2 += ($cnpj{$i} * $k);
-
-			if ($i < 12) {
-				$soma1 += ($cnpj{$i} * $j);
-			}
-
-			$k--;
-			$j--;
-
-		}
-
-		$digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
-		$digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
-
-		if((($cnpj{12} == $digito1) and ($cnpj{13} == $digito2)) == false){
-			echo "<b> CNPJ não valido </b>";
-			include "Formulario-cadastro-cliente.php";
-			die;
-		}
-
-}
-
     $f = fopen("Dados.csv","a");
     fputcsv($f, array($n, $data, $ender, $compl, $bairro,$cidade,$estado,$cep,$cnpj,$inscr,$tels,$celular,$email,$obs));
     fclose($f);
